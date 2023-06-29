9f12db70ccc17534bb28c740340c3692be64f33e
tmasuyama1114 <tmasuyama1114@gmail.com>
2023-06-29 13:44:26 +0900

    Add RSpec configuration to disable unnecessary test framework components

--- config/application.rb
+++ config/application.rb
@@ -36,6 +36,7 @@ module Innerlinker
     config.generators do |g|
       g.assets false
       g.helper false
+      g.test_framework :rspec, fixtures: false, view_specs: false, helper_specs: false, routing_specs: false
     end
   end
 end
